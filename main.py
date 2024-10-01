2a1fa513e95e5a3a0f33b5f6160bb214f6ba8638
ellencarlsson <98748113+ellencarlsson@users.noreply.github.com>
2024-10-01 15:20:50 +0200

    hoh

--- main.py
+++ main.py
@@ -4,30 +4,8 @@ import pygame
 
 mainpath = "DIMACS_graphs/"
 
-dimacs = "small_3"
+dimacs = "medium_2"
 dimacs_path = mainpath + dimacs + ".txt"
-
-"""def main():
-    global selected_node, stop_flag
-
-    # Initialize adjacency matrix and list
-    evolutionComputation.init_adj_matrix_and_list(dimacs_path)
-
-    # Start evolutionary graph coloring and get detailed results
-    results = evolutionComputation.evolutionary_graph_coloring()
-
-    # Log the results to the dynamically named text file
-    recordsHolder.log_results_to_file(results, dimacs)
-
-    # Update the master file with averages from all figures
-    recordsHolder.calculate_averages_from_log(dimacs)
-
-   
-
-if __name__ == "__main__":
-    for i in range(60):
-        main()"""
-
 
 def main():
     global selected_node, stop_flag
@@ -35,10 +13,10 @@ def main():
     # Initialize adjacency matrix and list
     evolutionComputation.init_adj_matrix_and_list(dimacs_path)
 
-    evolutionComputation.evolutionary_graph_coloring(dimacs)
-    #evolutionComputation.evolutionary_graph_coloring_min_colors(dimacs)
+    #evolutionComputation.evolutionary_graph_coloring(dimacs) #100
+    evolutionComputation.evolutionary_graph_coloring_min_colors(dimacs) #10
 
 
 if __name__ == "__main__":
-    for i in range(50):
+    for i in range(5):
         main()
